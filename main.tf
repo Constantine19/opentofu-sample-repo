02cd24bae10a33549e2b75f1d5fa4d11ebc1f64d
Konstantin Zelmanovich <konstantin.zelmanovich@env0.com>
2024-02-11 16:09:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "sample_bucket" {
   bucket = "sample-bucket-from-env0-opentofu"
 }
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.sample_bucket.s3_bucket_arn
+}
